dc4ebda34c92f37ef42665d505608ff5d7ab02ca
Tejaswini6-s <sutartejaswini28@gmail.com>
2026-08-05 21:14:41 +0530

    Add array_sum.c to calculate sum of four elements

---
+++ array_sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[4], i, sum = 0;
+
+    printf("Enter 4 elements:\n");
+
+    for(i = 0; i < 4; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("Sum of all elements = %d", sum);
+
+    return 0;
+}
