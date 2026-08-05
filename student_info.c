c3a5cb9dcdf2299b2857f60bb3d7b61510ec439f
Tejaswini6-s <sutartejaswini28@gmail.com>
2026-08-05 21:13:20 +0530

    Update student roll number and division

---
+++ student_info.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name: Tejaswini Sutar\n");
+    printf("Roll No: 16\n");
+    printf("Class: SY CSE\n");
+    printf("Division: c\n");
+
+    return 0;
+}
